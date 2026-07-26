1c51f4c980a812409d63f5ef0ed3675b0a6198ec
Lester Chan <lesterchan@gmail.com>
2026-07-26 19:51:54 +0800

    Apply WordPress Coding Standards (phpcbf)
    Whitespace only: tabs for indentation, spaces inside parens, brace placement.
    16 of 18 sniff violations fixed automatically. The two remaining (file docblock
    style, missing function docblock) are left for the restructure commit, which
    replaces this file's header wholesale.
    
    Playground harness reports an identical 4 PASS / 7 FAIL either side of this
    commit, confirming no behaviour changed.

--- freemyinternet.php
+++ freemyinternet.php
@@ -10,19 +10,19 @@ Author URI: http://freemyinternet.com/
 
 
 /*
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 // Prevent direct access.
@@ -35,8 +35,7 @@ defined( 'ABSPATH' ) || exit;
  * <title>, nav menus and widgets. A banner is not a title transformation; the
  * filter registration was simply wrong and is removed.
  */
-add_action('get_header', 'freemyinternet', 5);
-function freemyinternet()
-{
-    echo '<a style="width:100%;height:100%;vertical-align:middle;text-align:center;background-color:#000;position:absolute;z-index:8888;top:0px;left:0px;background-image:url(http://freemyinternet.com/free_my_internet.jpg);background-position:center center;background-repeat:no-repeat;" href="http://freemyinternet.com"></a>';
+add_action( 'get_header', 'freemyinternet', 5 );
+function freemyinternet() {
+	echo '<a style="width:100%;height:100%;vertical-align:middle;text-align:center;background-color:#000;position:absolute;z-index:8888;top:0px;left:0px;background-image:url(http://freemyinternet.com/free_my_internet.jpg);background-position:center center;background-repeat:no-repeat;" href="http://freemyinternet.com"></a>';
 }
